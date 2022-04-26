5affc85e9e87c44c66c08d80c80fa121c0dae8bd
Fabian Villasenor <exionite@gmail.com>
2022-04-26 09:20:04 -0700

    cards

--- src/Components/NewAtNoom/CoffeeMap.js
+++ src/Components/NewAtNoom/CoffeeMap.js
@@ -3,8 +3,47 @@
     image: "/images/NewatNoom/honduras.jpg",
     title: "Honduras Santa Elena Catracha Community",
     notes: "Toffee, honey, grapefruit",
-    price: "12oz | $15"
-  }
+    price: "12oz | $15",
+    origin: true,
+    originText: "Single origin"
+  },
+  {
+    image: "/images/NewatNoom/rwanda.jpg",
+    title: "Rwanda Nyanza Iwacu",
+    notes: "Candied mango, lemongrass, praline",
+    price: "12oz | $16",
+    origin: true,
+    originText: "Single origin"
+  },
+  {
+    image: "/images/NewatNoom/hayes.jpg",
+    title: "Hayes Valley Espresso",
+    notes: "Baking chocolate, orange zest, brown sugar",
+    price: "12oz | $21",
+    origin: true,
+    originText: "Espresso"
+  },
+  {
+    image: "/images/NewatNoom/grinder.jpg",
+    title: "Fellow Ode Grinder",
+    notes: "A next-level home grinder for brewed coffee, now in two colors",
+    price: "$299",
+    origin: false
+  },
+  {
+    image: "/images/NewatNoom/chemex.jpg",
+    title: "Chemex 6-Cup",
+    notes: "Same as it was in 1941, equal parts brilliance and utility",
+    price: "$47",
+    origin: false
+  },
+  {
+    image: "/images/NewatNoom/kettle.jpg",
+    title: "Balumda Kettle",
+    notes: "Ergonomically designed for flawless, effortless pours",
+    price: "$149",
+    origin: false
+  },
 ];
 
 export default coffeeData
--- src/Components/NewAtNoom/NewAtNoom.js
+++ src/Components/NewAtNoom/NewAtNoom.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./NewAtNoom.scss"
-import "./CoffeeMap"
 import Noom from './NoomCoffee/NoomCoffee'
 
 export default function NewAtNoom() {
