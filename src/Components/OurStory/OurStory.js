8e2b0397a9af968fa52309d36ee3fd5af217b286
Fabian Villasenor <exionite@gmail.com>
2022-04-26 14:10:14 -0700

    our story

--- src/Components/OurStory/OurStory.js
+++ src/Components/OurStory/OurStory.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import "./OurStory.scss"
+import { Button } from '../Styled/Styled'
 
 export default function OurStory() {
   return (
     <div className='ourstory-container'>
       <div className="ourstory-text">
-        text
+        <h1>Our Story...</h1>
+        <p>
+          Passion is what drives our purpose. We’ve been purchasing coffee directly from the farmers who grow it since 2007, when we were just a tiny, one cafe operation.
+        </p>
+        <p>
+          We’ve always roasted our coffees in small batches, and taste every roast because we’re obsessed with quality. We’re also a pioneer of the West Coast style of light roasting that continues to illuminate the spectrum of what coffee can be.
+        </p>
+        <Button className='ourstory-button'>
+          Learn More
+        </Button>
       </div>
       <div className="ourstory-photo">
         <img src="./story.png" alt="" />
