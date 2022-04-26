0e49074c278be5a22e81fc0230f044bc39a054f3
Fabian Villasenor <exionite@gmail.com>
2022-04-26 16:06:12 -0700

    brewguide

--- src/Components/BrewGuides/BrewGuides.js
+++ src/Components/BrewGuides/BrewGuides.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Header } from '../Styled/Styled'
 import "./BrewGuides.scss"
+import { brewGuideChemex, brewGuidePhotos } from '../../Reusable/Reusable'
 
 export default function BrewGuides() {
   return (
@@ -12,13 +13,20 @@ export default function BrewGuides() {
       </Header>
       <div className="guides">
         <div className="chemex-hero">
-          Chemex
+          <img src={brewGuideChemex} alt="" />
         </div>
         <div className="guide-right">
-          <div className="guides-card">1</div>
-          <div className="guides-card">2</div>
+          {brewGuidePhotos.map((photo) => (
+            <div className="guides-card" style={{
+              backgroundPosition: "50% 20%",
+              background:`linear-gradient(0deg, rgba(0, 0, 0, 0.39), rgba(0, 0, 0, 0.39)), url(${photo.image})`
+            }}
+              key={photo.id}>
+            </div>
+          ))}
+          {/* <div className="guides-card">2</div>
           <div className="guides-card">3</div>
-          <div className="guides-card">4</div>
+          <div className="guides-card">4</div> */}
         </div>
       </div>
     </Container>
--- src/Reusable/Reusable.js
+++ src/Reusable/Reusable.js
@@ -1,1 +1,20 @@
 export const DarkMoonLogo = "/images/NavbarImages/NoomCafeHeader.svg"
+export const brewGuidePhotos = [
+  {
+    image: "/images/BrewGuide/espresso-2.png",
+    id:0
+  },
+  {
+    image: "/images/BrewGuide/aeropress2.png",
+    id:1
+  },
+  {
+    image: "/images/BrewGuide/frenchpress2.png",
+    id:2
+  },
+  {
+    image: "/images/BrewGuide/milkart2.png",
+    id:3
+  }];
+
+export const brewGuideChemex = "/images/BrewGuide/guidechemex.png"
