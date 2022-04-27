927b3f46e84e94dfa8a1d35be8a901d1fb149027
Fabian Villasenor <exionite@gmail.com>
2022-04-27 10:25:42 -0700

    catch up photos

--- src/Components/CatchUp/CatchUp.js
+++ src/Components/CatchUp/CatchUp.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container, Header } from '../Styled/Styled'
+import { catchUpPhotos } from '../../Reusable/Reusable'
 import "./CatchUp.scss"
 
 export default function CatchUp() {
@@ -8,7 +9,15 @@ export default function CatchUp() {
       <Header>
         <h1>Catch up on our latest posts</h1>
       </Header>
-      
+      <div className="catchUp-photos-container">
+        {catchUpPhotos.map((catchUp) => (
+          <div className="catchUp-photo" id={catchUp.id}>
+            <img src={catchUp.image} alt="" />
+          </div>
+        ))}
+        {/* <div className="catchUp-photo">2</div>
+        <div className="catchUp-photo">3</div> */}
+      </div>
     </Container>
   )
 }
